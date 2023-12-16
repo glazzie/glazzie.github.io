c4e1ece48da4bca98c46f12a525d20bf5c45f6cc
Rasmus <rasse.andersson@live.se>
2023-12-16 21:34:49 +0100

    test

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:3000')
+
+</script>
+
+<template>
+  <div>
+    hej
+  </div>
+</template>
+
+<style scoped>
+</style>
